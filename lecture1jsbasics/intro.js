07145403d9bff918b6c68b794e018996d98e95fe
Mriga <mriga17chachra@gmail.com>
2021-01-30 12:02:47 +0530

    initial commit

---
+++ lecture1jsbasics/intro.js
@@ -0,0 +1,37 @@
+// no main function
+// no classes
+// no public static void main
+//console.log("hello world");
+
+
+if(true){
+    const a = 20;
+    if(true){
+
+    }
+}
+
+//let a=9;(output is 9)
+//let a; //(output is undefined)
+//console.log(a);
+
+let obj={
+    "name":"steve",
+    "skills":["martial arts","boxing","taekwando"],
+    "movies":["captain america","civil war","infinity war"],
+    "place":"queens, New york"
+};
+
+console.log(obj.place); //queens,new york
+
+let key="name";
+console.log(obj.key);//undefined
+
+console.log(obj[key]);//steve
+
+console.log(obj["name"]);//steve
+
+
+
+
+
